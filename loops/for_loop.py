0f7b36c14bce480dbbdc8f5c2de187a7b35fd4c5
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 13:03:44 +0500

    Create for_loop.py

---
+++ loops/for_loop.py
@@ -0,0 +1,9 @@
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+# Print each fruit in a fruit list:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# Loop through the letters in the word "banana":
+for x in "banana":
+  print(x)
