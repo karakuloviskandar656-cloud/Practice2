e17324c777366cd94a0aa2b35e4e3f61eec14891
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 13:07:21 +0500

    Add example of break statement in for loop
    Demonstrate the use of break statement in loops.

---
+++ loops/for_break.py
@@ -0,0 +1,15 @@
+# With the break statement we can stop the loop before it has looped through all the items:
+# Exit the loop when x is "banana"
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
