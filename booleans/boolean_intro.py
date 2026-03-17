d6de26454984e436618ca0dd3b82a025ccc97e76
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 12:16:15 +0500

    Enhance boolean_intro.py with examples and comments
    Added examples of boolean values and the bool() function.

--- booleans/boolean_intro.py
+++ booleans/boolean_intro.py
@@ -1,1 +1,25 @@
-print(True)
+# True and False this two thing called boolean values 
+print(10 > 9) # the output is True because ten is bigger then 9
+print(10 == 9) # the output ih False
+print(10 < 9) # Also False
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+
+print(bool("Hello"))
+print(bool(15))
+
+# Example
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
