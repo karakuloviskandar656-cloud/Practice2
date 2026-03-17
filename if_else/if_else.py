54eb7b20cea24f9e938ec4b2564cba1450ec3fca
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 12:34:08 +0500

    Add if-else statements for number and username checks

---
+++ if_else/if_else.py
@@ -0,0 +1,23 @@
+# Checking even or odd numbers:
+
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+x = 2
+if x > 3:
+    print("x is greater than 3")
+else:
+    print("x is not greater than 3")
+
+# Validating user input:
+
+username = "Emil"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
