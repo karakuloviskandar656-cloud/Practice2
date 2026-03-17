97e2799a0bd444514fb55b8da3bff55e7ea6bce3
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 12:19:04 +0500

    Create boolean_operators.py

---
+++ booleans/boolean_operators.py
@@ -0,0 +1,12 @@
+# Boolean operators
+print(True and False)   # False
+print(True or False)    # True
+print(not True)         # False
+# The following will return False:
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
